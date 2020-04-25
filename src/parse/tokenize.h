df04864e317b55073eb3282701065523c6e4b7c3
dosisod <39638017+dosisod@users.noreply.github.com>
2020-04-25 11:32:08 -0700

    added token classifier

--- src/parse/tokenize.h
+++ src/parse/tokenize.h
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TOKEN_UNKNOWN 0
+#define TOKEN_KEYWORD 1
+#define TOKEN_BRACKET_OPEN 2
+#define TOKEN_BRACKET_CLOSE 3
+
 //a token is text between whitespace/control characters
 typedef struct token_t {
 	unsigned long start;
 	unsigned long end;
+	int token_type;
 
 	struct token_t *next;
 } token_t;
@@ -19,6 +25,7 @@ token_t *tokenize(const char *code) {
 	struct token_t *head=malloc(sizeof(token_t));
 	head->start=-1;
 	head->end=-1;
+	head->token_type=TOKEN_UNKNOWN;
 	head->next=NULL;
 
 	struct token_t *current=head;
@@ -40,6 +47,7 @@ token_t *tokenize(const char *code) {
 				token_t *next_token=malloc(sizeof(token_t));
 				next_token->start=-1;
 				next_token->end=-1;
+				next_token->token_type=TOKEN_UNKNOWN;
 
 				last=current;
 				current->next=next_token;
@@ -59,4 +67,34 @@ token_t *tokenize(const char *code) {
 	}
 
 	return head;
+}
+
+//determine what to classify a single token as
+void classify_token(token_t *token, const char *code) {
+	int len=(token->end - token->start);
+	char buf[len + 1];
+
+	strncpy(buf, code + token->start, len);
+	buf[len]='\0';
+
+	if (strcmp(buf, "[")==0) {
+		token->token_type=TOKEN_BRACKET_OPEN;
+	}
+	else if (strcmp(buf, "]")==0) {
+		token->token_type=TOKEN_BRACKET_CLOSE;
+	}
+	//for now, the only known keyword is "return"
+	else if (strcmp(buf, "return")==0) {
+		token->token_type=TOKEN_KEYWORD;
+	}
+}
+
+//classify all tokens starting from head
+void classify_tokens(token_t *head, const char *code) {
+	token_t *current=head;
+
+	while (current!=NULL) {
+		classify_token(current, code);
+		current=current->next;
+	}
 }
--- test/parse/tokenize.h
+++ test/parse/tokenize.h
@@ -35,11 +35,76 @@ bool test_whitespace_at_eol_ignored() {
 	return !pass;
 }
 
+bool test_bracket_token_open() {
+	const char *code="[";
+	token_t *t=tokenize(code);
+	classify_token(t, code);
+
+	bool pass=(t->token_type==TOKEN_BRACKET_OPEN);
+	free(t);
+
+	return !pass;
+}
+
+bool test_bracket_token_close() {
+	const char *code="]";
+	token_t *t=tokenize(code);
+	classify_token(t, code);
+
+	bool pass=(t->token_type==TOKEN_BRACKET_CLOSE);
+	free(t);
+
+	return !pass;
+}
+
+bool test_token_keyword() {
+	const char *code="return";
+	token_t *t=tokenize(code);
+	classify_token(t, code);
+
+	bool pass=(t->token_type==TOKEN_KEYWORD);
+	free(t);
+
+	return !pass;
+}
+
+bool test_token_unknown() {
+	const char *code="garbage_value";
+	token_t *t=tokenize(code);
+	classify_token(t, code);
+
+	bool pass=(t->token_type==TOKEN_UNKNOWN);
+	free(t);
+
+	return !pass;
+}
+
+bool test_token_classifier() {
+	const char *code="[ ]";
+	token_t *t=tokenize(code);
+	classify_tokens(t, code);
+
+	bool pass=(
+		t->token_type==TOKEN_BRACKET_OPEN &&
+		t->next->token_type==TOKEN_BRACKET_CLOSE
+	);
+
+	free(t->next);
+	free(t);
+
+	return !pass;
+}
+
 void tokenizer_test_self(bool *failed) {
 	tests_t tests={
 		test_tokenize_single_token,
 		test_whitespace_between_tokens,
 		test_whitespace_at_eol_ignored,
+		test_bracket_token_open,
+		test_bracket_token_close,
+		test_token_keyword,
+		test_token_unknown,
+		test_token_classifier,
 		NULL
 	};
 
