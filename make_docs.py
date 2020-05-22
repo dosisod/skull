aa6d7ba08a6d0ab0ccc0e5b114626445ba7ef216
dosisod <39638017+dosisod@users.noreply.github.com>
2020-05-21 21:31:07 -0700

    condensed make_docs code

--- make_docs.py
+++ make_docs.py
@@ -1,35 +1,21 @@
 from pathlib import Path
 import re
 
+re_comment=re.compile(r"(\/\*[\s\S]*?\*\/)")
 
 def display_markdown(code):
-	re_comment=re.compile(r"(\/\*[\s\S]*?\*\/)")
-
 	ret=""
-
 	for comment in re_comment.finditer(code):
-		start=comment.start()
 		end=comment.end()
 
 		ret+="#### `" + code[end:].split("\n")[1][:-2] + "`\n"
-		ret+=code[start+3:end-3] + "\n\n"
+		ret+=code[comment.start() + 3:end - 3] + "\n\n"
 
 	return ret
 
-src_path=Path("./src")
-
-sources=[p for p in src_path.rglob("*") if p.suffix in [".c", ".h"]]
-parts_len=len(src_path.resolve().parts)
-
-for source in sources:
-	doc_path=Path("docs") / "/".join(source.resolve().parts[parts_len:])
-
-	with open(source, "r") as f:
+for source in [src for src in Path("./src").rglob("*") if src.suffix in [".c", ".h"]]:
+	with source.open() as f:
 		markdown=display_markdown(f.read())
-
-		if not markdown:
-			continue
-
-		with open(str(doc_path)[:-2]+".md", "w+") as ff:
-			ff.write(f"# {source}\n\n")
-			ff.write(markdown)
+		if markdown:
+			with open(str(Path("docs") / "/".join(source.parts[1:]))[:-2] + ".md", "w+") as ff:
+				ff.write(f"# {source}\n\n{markdown}")
