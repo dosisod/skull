0c0da71f7c31c49f39197c982074ad50b5522c5d
dosisod <39638017+dosisod@users.noreply.github.com>
2020-05-26 20:17:53 -0700

    repl can read from commandline now

--- docs/eval/repl.md
+++ docs/eval/repl.md
@@ -1,5 +1,8 @@
 # src/eval/repl.h
 
+#### `wchar_t *repl_read()`
+Returns pointer to string read from commandline.
+
 #### `wchar_t *repl_eval(wchar_t *str)`
 Evaluates a single line, returns result as a string (if any).
 
--- src/eval/repl.h
+++ src/eval/repl.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include <stdio.h>
+
+#include "../common/color.h"
 #include "../parse/classify.h"
 #include "../parse/tokenize.h"
+
+#define REPL_MAX_LINE_LEN 1024
+
+/*
+Returns pointer to string read from commandline.
+*/
+wchar_t *repl_read() {
+	wchar_t *line=malloc(REPL_MAX_LINE_LEN * sizeof(wchar_t));
+
+	wprintf(COLOR_BRIGHT_GREEN_FG L"> " COLOR_RESET);
+	fgetws(line, REPL_MAX_LINE_LEN, stdin);
+	line[wcscspn(line, L"\n")]=0;
+
+	return line;
+}
 
 /*
 Evaluates a single line, returns result as a string (if any).
