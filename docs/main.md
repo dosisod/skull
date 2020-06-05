fab64ce10f9da0a5343ca69ed4949d2f7124eb19
dosisod <39638017+dosisod@users.noreply.github.com>
2020-06-05 00:34:30 -0700

    added version to repl, removed -i flag

--- docs/main.md
+++ docs/main.md
@@ -1,7 +1,7 @@
 # src/main.c
 
 #### `int main(int argc, char *argv[])`
-If `-i` option is set, run in interactive (repl) mode.
+If no parameters are passed, run in interactive (repl) mode.
 
-Else, run the passed `.skull` file.
+Else, run a passed in `.skull` file.
 
--- src/main.c
+++ src/main.c
@@ -4,22 +4,22 @@
 #include "../src/eval/repl.h"
 #include "../src/eval.h"
 
-/*
-If `-i` option is set, run in interactive (repl) mode.
+#define SKULL_VERSION "0.0.1"
 
-Else, run the passed `.skull` file.
+/*
+If no parameters are passed, run in interactive (repl) mode.
+
+Else, run a passed in `.skull` file.
 */
 int main(int argc, char *argv[]) {
 	if (!setlocale(LC_CTYPE, "")) {
 		wprintf(L"Could not set locale.");
 		return 1;
 	}
-	if (argc!=2) {
-		wprintf(L"no input files specified, exiting\n");
-		return 1;
-	}
 
-	if (strcmp(argv[1], "-i")==0) {
+	if (argc==1) {
+		wprintf(L"Skull " SKULL_VERSION "\n");
+
 		context_t *ctx=make_context();
 		make_default_types();
 
@@ -35,18 +35,19 @@ int main(int argc, char *argv[]) {
 			free(line);
 		}
 	}
-	else {
-		FILE *f;
-		f=fopen(argv[1], "r");
 
-		if (f==NULL) {
-			wprintf(L"\"%s\" was not found, exiting\n", argv[1]);
-			return 1;
-		}
-
-		eval(f);
-		fclose(f);
+	else if (argc>2) {
+		wprintf(L"no input files specified, exiting\n");
+		return 1;
 	}
 
+	FILE *f=fopen(argv[1], "r");
+	if (f==NULL) {
+		wprintf(L"\"%s\" was not found, exiting\n", argv[1]);
+		return 1;
+	}
+
+	eval(f);
+	fclose(f);
 	return 0;
 }
