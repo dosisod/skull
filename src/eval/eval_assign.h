efae0b6a0116d1e56c5706a9db5f1bbdadba3eac
dosisod <39638017+dosisod@users.noreply.github.com>
2020-06-21 17:53:16 -0700

    replaced references to stack variables with mallocated versions

--- src/eval/eval_assign.h
+++ src/eval/eval_assign.h
@@ -7,6 +7,12 @@
 #include "eval_integer.h"
 #include "variable.h"
 
+#define SETUP_MEM(name, type, func) \
+	const type tmp=(func); \
+	mem=malloc(sizeof(type)); \
+	DIE_IF_MALLOC_FAILS(mem); \
+	memcpy((void*)mem, &tmp, sizeof(type));
+
 /*
 Assign the value of `token` to a variable `var`.
 
@@ -15,9 +21,6 @@ Set `ctx` to allow for assigning variables to other variables.
 Return an error (as a string) if any occured, else `NULL`.
 */
 const char32_t *eval_assign(variable_t *var, token_t *token, const context_t *ctx) {
-	const void *mem=NULL;
-	const char32_t *err=NULL;
-
 	if (ctx!=NULL && token->token_type==TOKEN_IDENTIFIER) {
 		MAKE_TOKEN_BUF(buf, token);
 		variable_t *var_new=context_find_name(ctx, buf);
@@ -29,27 +32,27 @@ const char32_t *eval_assign(variable_t *var, token_t *token, const context_t *ct
 			return ERROR_MSG[ERROR_TYPE_MISMATCH];
 		}
 
-		uint8_t mem_tmp[var_new->bytes];
-		variable_read(mem_tmp, var_new);
-		variable_write(var, mem_tmp);
+		uint8_t mem[var_new->bytes];
+		variable_read(mem, var_new);
+		variable_write(var, mem);
 
 		return NULL;
 	}
 
+	const void *mem=NULL;
+	const char32_t *err=NULL;
+
 	if (var->type==find_type(U"int") && token->token_type==TOKEN_INT_CONST) {
-		int64_t tmp=eval_integer(token, &err);
-		mem=&tmp;
+		SETUP_MEM(mem, int64_t, eval_integer(token, &err));
 	}
 	else if (var->type==find_type(U"float") && token->token_type==TOKEN_FLOAT_CONST) {
-		long double tmp=eval_float(token, &err);
-		mem=&tmp;
+		SETUP_MEM(mem, long double, eval_float(token, &err));
 	}
 	else if (var->type==find_type(U"bool") && token->token_type==TOKEN_BOOL_CONST) {
-		bool tmp=token_cmp(U"true", token);
-		mem=&tmp;
+		SETUP_MEM(mem, bool, token_cmp(U"true", token));
 	}
 	else if (var->type==find_type(U"char") && token->token_type==TOKEN_CHAR_CONST) {
-		mem=token->begin;
+		SETUP_MEM(mem, char32_t, *token->begin);
 	}
 	else if (var->type==find_type(U"str") && token->token_type==TOKEN_STR_CONST) {
 		char32_t *current=NULL;
@@ -60,30 +63,34 @@ const char32_t *eval_assign(variable_t *var, token_t *token, const context_t *ct
 		}
 
 		MAKE_TOKEN_BUF(buf, token);
-		char32_t *tmp=c32sdup(buf);
-		DIE_IF_MALLOC_FAILS(tmp);
-
-		mem=&tmp;
+		SETUP_MEM(mem, char32_t*, c32sdup(buf));
 	}
 	else {
 		return ERROR_MSG[ERROR_TYPE_MISMATCH];
 	}
 
 	if (err!=NULL) {
+		free((void*)mem);
 		if (var->type==find_type(U"str")) {
-			mem=NULL;
-			variable_read(&mem, var);
-			free((char32_t*)mem);
+			char32_t *str=NULL;
+			variable_read(&str, var);
+			free(str);
 		}
 		return err;
 	}
+
 	if (variable_write(var, mem)==ERROR_MSG[ERROR_CANNOT_ASSIGN_CONST]) {
+		free((void*)mem);
 		if (var->type==find_type(U"str")) {
-			mem=NULL;
-			variable_read(&mem, var); // NOLINT
-			free((char32_t*)mem);
+			char32_t *str=NULL;
+			variable_read(&str, var); // NOLINT
+			free(str);
 		}
 		return ERROR_MSG[ERROR_CANNOT_ASSIGN_CONST]; // NOLINT
+	}
+
+	if (var->type!=find_type(U"str")) {
+		free((void*)mem);
 	}
 	return NULL; // NOLINT
 }
