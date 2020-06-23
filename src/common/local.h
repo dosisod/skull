0f4380bfa5c2a663758ea1f1738b91e74f842feb
dosisod <39638017+dosisod@users.noreply.github.com>
2020-06-22 18:19:05 -0700

    added UTF-32 sanity check

--- src/common/local.h
+++ src/common/local.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <assert.h>
 #include <locale.h>
 
 //if system local is not set, default to "en_US.UTF8"
+//throw assertion if system does not support UTF-32
+
+#ifndef __STDC_UTF_32__
+#define __STDC_UTF_32__ 0
+#endif
 
 #define SETUP_LOCALE() \
+	static_assert(__STDC_UTF_32__, "Your system does not support UTF-32 for 32 bit char types, we cannot ensure Unicode operations will be handled correctly"); \
 	const char *loc=""; \
 	if (strcmp("C", setlocale(LC_CTYPE, ""))==0) { \
 		loc="en_US.UTF8"; \
