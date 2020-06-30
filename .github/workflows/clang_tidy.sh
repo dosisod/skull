fcd433dc93f2752d64bedce36fba4165a51c7b62
dosisod <39638017+dosisod@users.noreply.github.com>
2020-06-30 13:11:20 -0700

    added skull unit tests to CI

--- .github/workflows/clang_tidy.sh
+++ .github/workflows/clang_tidy.sh
@@ -1,3 +1,3 @@
-if [[ $(clang-tidy -checks=*,-llvm-header-guard $(find -L {src,test} -type f | grep -E "(c|h)$") -quiet -- -std=c11 2>&1 | tee /dev/stderr | grep "skull") ]]; then
+if [[ $(clang-tidy -checks=*,-llvm-header-guard $(find -L {src,test} -type f | grep -E "\.(c|h)$") -quiet -- -std=c11 2>&1 | tee /dev/stderr | grep "skull") ]]; then
 	exit 1
 fi
---
+++ test/sh/main.sh
@@ -0,0 +1,19 @@
+pass=true
+
+test() {
+	echo -n "$1 "
+	./build/src/skull ./test/sh/$1.skull
+	if [ "$?" -ne "$2" ]; then
+		/bin/echo -e "\033[91mFAIL\033[0m\n"
+		pass=false
+	else
+		/bin/echo -e "\033[92mPASS\033[0m"
+	fi
+}
+
+echo "Running Skull unit tests"
+
+test "exit_code_0" 0
+test "exit_code_1" 1
+
+$pass || (echo "1 or more tests failed" && exit 1)
