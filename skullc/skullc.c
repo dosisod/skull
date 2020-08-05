d2380d334533972be8362a3ffd3bb138a69fa50d
dosisod <39638017+dosisod@users.noreply.github.com>
2020-08-04 22:41:08 -0700

    skullc now reads from file

--- skullc/skullc.c
+++ skullc/skullc.c
@@ -1,9 +1,13 @@
+#include <errno.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #include <llvm-c/Core.h>
 
+#include "skull/common/malloc.h"
 #include "skull/eval/eval_integer.h"
+#include "skull/eval/repl.h"
 #include "skull/parse/ast/node.h"
 
 int main(void) {
@@ -36,7 +40,39 @@ int main(void) {
 		block
 	);
 
-	ast_node_t *node=make_ast_tree(U"return 0");
+	errno=0;
+	FILE *fd=fopen("main.sk", "re");
+	if (fd==NULL) {
+		puts("error opening file");
+		return 1;
+	}
+
+	char *str=malloc(sizeof(char) * REPL_MAX_LINE_LEN);
+	DIE_IF_MALLOC_FAILS(str);
+
+	size_t offset=0;
+	int c=getc(fd);
+
+	while (c!=EOF) {
+		//read char by char until we need to reallocate more memory
+		if (offset!=0 && ((offset + 1) % REPL_MAX_LINE_LEN)==0) {
+			char *new_str=realloc(str, sizeof(char) * (offset + REPL_MAX_LINE_LEN));
+			DIE_IF_MALLOC_FAILS(new_str);
+
+			str=new_str;
+		}
+		str[offset]=(char)c;
+		offset++;
+
+		c=getc(fd);
+	}
+	str[offset]='\0';
+
+	char32_t *file_str=mbstoc32s(str);
+	free(str);
+
+	ast_node_t *node=make_ast_tree(file_str);
+	free(file_str);
 
 	if (node->node_type==AST_NODE_RETURN) {
 		const char32_t *error = NULL;
