19acb7809173141296274ae5a11687aa51f3f3f0
dosisod <39638017+dosisod@users.noreply.github.com>
2020-12-21 21:55:35 -0800

    fix clang_tidy script in pre-commit silently failing

--- .github/workflows/clang_tidy.sh
+++ .github/workflows/clang_tidy.sh
@@ -1,9 +1,10 @@
 CLANG_TIDY=$(command -v clang-tidy-9 || command -v clang-tidy)
+LLVM_CONFIG=$(command -v llvm-config-9 || command -v llvm-config)
 
 [ ! $($CLANG_TIDY \
 	$(find -L skull test -type f | grep -E "\.(c|h)$") \
 	-checks=*,-hicpp-signed-bitwise,-llvm-header-guard,-readability-named-parameter,-readability-avoid-const-params-in-decls,-google-readability-avoid-underscore-in-googletest-name,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-cppcoreguidelines-avoid-magic-numbers,-readability-magic-numbers \
 	-header-filter=.* \
 	-quiet -- \
-	-std=c17 -I. $(llvm-config-9 --cflags) 2>&1 | tee /dev/stderr | grep "skull"
+	-std=c17 -I. $($LLVM_CONFIG --cflags) 2>&1 | tee /dev/stderr | grep "skull"
 ) ]
