4771d4ead0da4515d1d575258b5a2b1b6fbd28bc
dosisod <39638017+dosisod@users.noreply.github.com>
2020-12-21 21:47:06 -0800

    add fallback to clang-tidy and llc if explicit version 9 doesnt exist

--- .github/workflows/clang_tidy.sh
+++ .github/workflows/clang_tidy.sh
@@ -1,4 +1,6 @@
-[ ! $(clang-tidy-9 \
+CLANG_TIDY=$(command -v clang-tidy-9 || command -v clang-tidy)
+
+[ ! $($CLANG_TIDY \
 	$(find -L skull test -type f | grep -E "\.(c|h)$") \
 	-checks=*,-hicpp-signed-bitwise,-llvm-header-guard,-readability-named-parameter,-readability-avoid-const-params-in-decls,-google-readability-avoid-underscore-in-googletest-name,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-cppcoreguidelines-avoid-magic-numbers,-readability-magic-numbers \
 	-header-filter=.* \
