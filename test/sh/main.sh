18b0f55d0865925d28141cd065b47c510e859184
dosisod <39638017+dosisod@users.noreply.github.com>
2021-01-20 21:35:53 -0800

    Auto detect/run tests that are in the test directory

--- test/sh/main.sh
+++ test/sh/main.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 pass=true
 
 sha() {
@@ -26,15 +24,31 @@ pass_or_fail() {
 	[ "$1" = "0" ] && fail || pass
 }
 
-test() {
-	echo -n "$1 $2 "
+test_normal() {
+	dir=$(dirname "$1")
+	file=$(basename "$1")
+	file="$(expr substr "${file%.*}" 2 99999)"
 
-	rm -f ./test/sh/$1/.$2.ll
-	./build/skull/_skull ./test/sh/$1/$2
+	echo -n "$dir/$file "
 
-	cmp ./test/sh/$1/_$2.ll ./test/sh/$1/.$2.ll
+	rm -f "./$dir/.$file.ll"
+	./build/skull/_skull "./$dir/$file"
 
-	rm -f ./test/sh/$1/.$2.ll
+	cmp "./$dir/_$file.ll" "./$dir/.$file.ll"
+	rm -f "./$dir/.$file.ll"
+}
+
+test_error() {
+	dir=$(dirname "$1")
+	file=$(basename "$1")
+	file="$(expr substr "${file%.*}" 2 99999)"
+
+	echo -n "$dir/$file "
+
+	./build/skull/_skull "./test/sh/error/$file" > "./test/sh/error/.$file.out"
+
+	cmp "./test/sh/error/.$file.out" "./test/sh/error/_$file.out"
+	rm -f "./test/sh/error/.$file.out"
 }
 
 test_option() {
@@ -44,22 +58,12 @@ test_option() {
 	pass_or_fail $?
 }
 
-test_error() {
-	echo -n "error $1 "
-
-	./build/skull/_skull ./test/sh/error/$1 > ./test/sh/error/.$1.out
-
-	cmp ./test/sh/error/.$1.out ./test/sh/error/_$1.out
-
-	rm -f ./test/sh/error/.$1.out
-}
-
 test_skull() {
 	echo -n "skull $1 "
 
 	out=$(./build/skull/skull $2)
 
-	sh -e ./test/sh/skull/$1 "$out" "$?"
+	sh -e "./test/sh/skull/$1" "$out" "$?"
 	[ "$?" != "0" ]
 	pass_or_fail $?
 }
@@ -68,185 +72,25 @@ echo
 echo "Running Skull unit tests"
 echo
 
-test "declare" "bool.sk"
-test "declare" "float_infinity.sk"
-test "declare" "float_negative_infinity.sk"
-test "declare" "float_negative.sk"
-test "declare" "float.sk"
-test "declare" "float_zero.sk"
-test "declare" "int_binary.sk"
-test "declare" "int_hex.sk"
-test "declare" "int_negative.sk"
-test "declare" "int_octal.sk"
-test "declare" "int.sk"
-test "declare" "many.sk"
-test "declare" "mut_explicit.sk"
-test "declare" "mut.sk"
-test "declare" "rune_escaped.sk"
-test "declare" "rune_null.sk"
-test "declare" "rune.sk"
-test "declare" "str.sk"
-test "declare" "escape_sequences.sk"
-test "declare" "variable_auto.sk"
-test "declare" "reassign_str.sk"
-test "declare" "reassign_var.sk"
-test "declare/bool_expr" "is.sk"
-test "declare/bool_expr" "not.sk"
-test "declare/bool_expr" "var_rhs.sk"
-test "declare/bool_expr" "and.sk"
-test "declare/bool_expr" "or.sk"
-test "declare/bool_expr" "xor.sk"
-test "declare/bool_expr" "less_than.sk"
-test "declare/bool_expr" "gtr_than_eq.sk"
-test "declare/bool_expr" "less_than.sk"
-test "declare/bool_expr" "gtr_than_eq.sk"
-test "declare/bool_expr" "is_not.sk"
+for file in $(find test/sh/ -name "_*.ll") ; do
+	test_normal "$file"
+done
 
-test "oper" "add_float_consts.sk"
-test "oper" "add_int_consts.sk"
-test "oper" "mult_float_consts.sk"
-test "oper" "mult_int_consts.sk"
-test "oper" "sub_float_consts.sk"
-test "oper" "sub_int_consts.sk"
-test "oper" "div_float_consts.sk"
-test "oper" "div_int_consts.sk"
-test "oper" "lhs_with_var.sk"
-test "oper" "rhs_with_var.sk"
+for file in $(find test/sh/error/ -name "_*.out") ; do
+	test_error "$file"
+done
 
-test "function" "assign_return_value.sk"
-test "function" "call_str.sk"
-test "function" "single_param.sk"
-test "function" "var_param.sk"
-test "function" "external_return_type.sk"
-test "function" "external_with_param.sk"
-test "function" "multiple_externals.sk"
-test "function" "nodes_after_call_not_ignored.sk"
-test "function" "simple.sk"
-test "function" "declare_func.sk"
-test "function" "func_with_scope.sk"
-test "function" "multi_var_func.sk"
-test "function" "nested_if.sk"
-test "function" "exported.sk"
-test "function" "non_int_return.sk"
-test "function" "expr_param.sk"
-test "function" "multi_param_func.sk"
-test "function" "multi_param_func_different_types.sk"
-
-test "flow/if" "empty_body.sk"
-test "flow/if" "false.sk"
-test "flow/if" "true.sk"
-test "flow/if" "with_var_false.sk"
-test "flow/if" "with_var_true.sk"
-test "flow/if" "else_comment.sk"
-test "flow/if" "not_with_const.sk"
-test "flow/if" "not_with_var.sk"
-
-test "flow" "else.sk"
-test "flow" "elif.sk"
-test "flow" "set_var_in_if.sk"
-test "flow" "return_0.sk"
-test "flow" "return_var.sk"
-test "flow" "while.sk"
-test "flow" "nested_if.sk"
-test "flow" "is_cmp_int.sk"
-test "flow" "is_cmp_float.sk"
-test "flow" "is_cmp_rune.sk"
-test "flow" "return_expr.sk"
-test "flow" "return_func_value.sk"
-
-test "misc" "const_inline.sk"
-test "misc" "ignore_comments.sk"
-test "misc" "ignore_block_comments.sk"
-test "misc" "no_eol_works.sk"
-test "misc" "locale.sk"
-test "misc" "comment_then_code.sk"
-test "misc" "type_alias.sk"
-test "misc" "preserve_nested_scopes.sk"
+touch test/sh/error/read_protected.sk
+chmod 200 test/sh/error/read_protected.sk
+echo "cannot open \"./test/sh/error/read_protected.sk\", permission denied" > test/sh/error/_read_protected.sk.out
+test_error "test/sh/error/_read_protected.sk.out"
+rm test/sh/error/_read_protected.sk.out
+rm test/sh/error/read_protected.sk
 
 test_option "missing_file_extension_fails" "missing required \".sk\" extension, exiting"
 test_option ".sk" "\".sk\" is not a valid name, exiting"
 test_option "not_a_file.sk" "\"./test/sh/not_a_file.sk\" was not found, exiting"
 test_option "option1\ option2\ option3" "unexpected number of parameters"
-
-test_error "var_already_defined.sk"
-test_error "return_int_overflow.sk"
-test_error "declare_int_overflow.sk"
-test_error "declare_int_underflow.sk"
-test_error "declare_float_overflow.sk"
-test_error "illegal_utf8.sk"
-test_error "assign_missing_rhs.sk"
-test_error "auto_assign_missing_rhs.sk"
-test_error "assign_unknown_var.sk"
-test_error "assign_unassignable_expression.sk"
-test_error "return_non_existent_var.sk"
-test_error "mismatched_bracket.sk"
-test_error "stray_close_bracket.sk"
-test_error "extra_close_bracket.sk"
-test_error "no_closing_bracket.sk"
-test_error "invalid_return_if.sk"
-test_error "if_with_nonexisting_var.sk"
-test_error "only_identifier_fails.sk"
-test_error "if_var_out_of_scope.sk"
-test_error "if_missing_block.sk"
-test_error "reassign_non_existet_var.sk"
-test_error "reassign_const_var.sk"
-test_error "reassign_missing_rhs.sk"
-test_error "unexpected_str_fails.sk"
-test_error "add_mismatched_consts.sk"
-test_error "sub_mismatched_consts.sk"
-test_error "mult_mismatched_consts.sk"
-test_error "div_mismatched_consts.sk"
-test_error "div_by_zero.sk"
-test_error "assign_mismatch_var_type.sk"
-test_error "missing_external.sk"
-test_error "redeclare_external.sk"
-test_error "no_newline_external.sk"
-test_error "no_content.sk"
-test_error "escape_missing_control.sk"
-test_error "escape_hex_missing_both.sk"
-test_error "escape_hex_missing_second.sk"
-test_error "escape_hex_first_bad.sk"
-test_error "escape_hex_second_bad.sk"
-test_error "function_bad_param_type.sk"
-test_error "function_param_unknown_var.sk"
-test_error "function_param_bad_type.sk"
-test_error "function_shadowed_param.sk"
-test_error "assign_void_function.sk"
-test_error "function_missing_return.sk"
-test_error "function_unexpected_return.sk"
-test_error "function_missing_declaration.sk"
-test_error "oper_rhs_var_not_found.sk"
-test_error "oper_rhs_var_bad_type.sk"
-test_error "oper_lhs_var_not_found.sk"
-test_error "oper_lhs_var_bad_type.sk"
-test_error "redundant_assign.sk"
-test_error "closing_block_comment_missing.sk"
-test_error "unexpected_code_block.sk"
-test_error "else_without_if.sk"
-test_error "unreachable_return.sk"
-test_error "invalid_identifier.sk"
-test_error "main_reserved.sk"
-test_error "non_int_return.sk"
-test_error "passing_param_to_paramless_func.sk"
-test_error "non_bool_cond.sk"
-test_error "no_nested_export.sk"
-test_error "func_assign_bad_type.sk"
-test_error "expr_assign_bad_type.sk"
-test_error "bad_expr_func_param.sk"
-test_error "multi_param_func_missing_comma.sk"
-test_error "bad_type_not_allowed.sk"
-test_error "no_same_name_aliases.sk"
-test_error "function_proto_param_bad_type.sk"
-test_error "function_proto_return_bad_type.sk"
-test_error "return_missing_expr.sk"
-test_error "bool_expr_and_not_bool.sk"
-test_error "bool_expr_or_not_bool.sk"
-test_error "bool_expr_xor_not_bool.sk"
-
-touch test/sh/error/read_protected.sk
-chmod 200 test/sh/error/read_protected.sk
-test_error "read_protected.sk"
-rm test/sh/error/read_protected.sk
 
 test_skull "version.sh" "-v"
 test_skull "help.sh" "-h"
