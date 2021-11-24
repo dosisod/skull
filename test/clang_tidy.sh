b8f290c16a67162bca8897c6facc04050b221f04
dosisod <39638017+dosisod@users.noreply.github.com>
2021-11-23 22:43:46 -0800

    Add e2e tests for C backend, fix clang tidy parsing auto generated code

--- test/clang_tidy.sh
+++ test/clang_tidy.sh
@@ -4,7 +4,7 @@ CLANG_TIDY=$(command -v clang-tidy-10 || command -v clang-tidy)
 LLVM_CONFIG=$(command -v llvm-config-10 || command -v llvm-config)
 
 [ ! "$($CLANG_TIDY \
-	$(find -L skull test -type f | grep -E "\.(c|h)$") \
+	$(find -L skull test -type f -name "*.[ch]" ! -name "*.sk.c") \
 	-checks=*,-google-readability-braces-around-statements,-readability-braces-around-statements,-hicpp-braces-around-statements,-hicpp-signed-bitwise,-llvm-header-guard,-readability-named-parameter,-readability-avoid-const-params-in-decls,-google-readability-avoid-underscore-in-googletest-name,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-cppcoreguidelines-avoid-magic-numbers,-readability-magic-numbers,-cppcoreguidelines-init-variables \
 	-header-filter=.* \
 	-quiet -- \
--- test/skull/codegen/c/_first.sk.c
+++ test/skull/codegen/c/_first.sk.c
@@ -6,6 +6,18 @@ static int init(void) {
 	int64_t x = 1;
 	x = 2;
 	(void)0;
+	if (1) {
+		(void)0;
+	}
+	else if (1) {
+		(void)0;
+	}
+	else {
+		(void)0;
+	}
+	while (0) {
+		(void)0;
+	}
 	return x;
 	return 0;
 }
