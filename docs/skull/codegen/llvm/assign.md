30b19f3c396183f197be95f4f829f27c2f0ef561
dosisod <39638017+dosisod@users.noreply.github.com>
2021-12-22 17:48:56 -0800

    Cleanup LLVM assign code

--- docs/skull/codegen/llvm/assign.md
+++ docs/skull/codegen/llvm/assign.md
@@ -13,8 +13,17 @@ void gen_stmt_var_assign(const AstNodeVarAssign *var_assign)
 > Assign a to a variable from `var_assign`.
 
 ```c
-static void assign_value_to_var(LLVMValueRef value, Variable *var, bool is_first_assign)
+static void assign_value_to_var(LLVMValueRef value, Variable *var)
 ```
 
 > Assign `value` to `var`.
+
+```c
+static bool requires_global_decl(const Variable *var)
+```
+
+> Determine if a global LLVM declaration is needed for this variable.
+> \
+> Exported variables always need a declaration, and mutable variables only
+> need one if they are marked "mut" (since they could be reassigned later).
 
--- skull/codegen/llvm/assign.c
+++ skull/codegen/llvm/assign.c
@@ -15,8 +15,11 @@
 
 #include "skull/codegen/llvm/assign.h"
 
-static void assign_value_to_var(LLVMValueRef, Variable *, bool);
+static void assign_value_to_var(LLVMValueRef, Variable *);
 static void setup_var_llvm(LLVMValueRef, Variable *);
+static bool is_top_level(void);
+static bool requires_global_decl(const Variable *);
+static bool requires_alloca_decl(const Variable *);
 
 /*
 Builds a variable definition from `var_def`.
@@ -30,7 +33,6 @@ void gen_stmt_var_def(const AstNodeVarDef *var_def) {
 	LLVMValueRef value = gen_expr(var_def->expr).value;
 
 	setup_var_llvm(value, var);
-	assign_value_to_var(value, var, true);
 	add_llvm_var_def_debug_info(var);
 }
 
@@ -40,17 +42,16 @@ Assign a to a variable from `var_assign`.
 void gen_stmt_var_assign(const AstNodeVarAssign *var_assign) {
 	LLVMValueRef value = gen_expr(var_assign->expr).value;
 
-	assign_value_to_var(value, var_assign->var, false);
+	assign_value_to_var(value, var_assign->var);
 }
 
 static void setup_var_llvm(LLVMValueRef value, Variable *var) {
 	const bool is_const_literal = LLVMIsConstant(value);
-	const bool is_export = var->is_exported;
 
-	const bool is_global = \
-		SKULL_STATE_LLVM.current_func == SKULL_STATE_LLVM.main_func;
+	var->is_global = is_top_level();
+	var->is_const_lit = is_const_literal;
 
-	if (is_global && (!var->is_const || !is_const_literal || is_export)) {
+	if (requires_global_decl(var)) {
 		var->ref = LLVMAddGlobal(
 			SKULL_STATE_LLVM.module,
 			type_to_llvm_type(var->type),
@@ -59,7 +60,7 @@ static void setup_var_llvm(LLVMValueRef value, Variable *var) {
 
 		LLVMSetLinkage(
 			var->ref,
-			is_export ? LLVMExternalLinkage : LLVMPrivateLinkage
+			var->is_exported ? LLVMExternalLinkage : LLVMPrivateLinkage
 		);
 
 		LLVMSetInitializer(
@@ -68,8 +69,10 @@ static void setup_var_llvm(LLVMValueRef value, Variable *var) {
 				value :
 				LLVMConstNull(type_to_llvm_type(var->type))
 		);
+
+		if (is_const_literal) return;
 	}
-	else if (!is_global && !var->is_const) {
+	else if (requires_alloca_decl(var)) {
 		var->ref = LLVMBuildAlloca(
 			SKULL_STATE_LLVM.builder,
 			type_to_llvm_type(var->type),
@@ -77,28 +80,18 @@ static void setup_var_llvm(LLVMValueRef value, Variable *var) {
 		);
 	}
 
-	var->is_global = is_global;
-	var->is_const_lit = is_const_literal;
+	assign_value_to_var(value, var);
 }
 
 /*
 Assign `value` to `var`.
 */
-static void assign_value_to_var(
-	LLVMValueRef value,
-	Variable *var,
-	bool is_first_assign
-) {
-	const bool is_const_literal = LLVMIsConstant(value);
-
-	const bool is_global = is_first_assign ?
-		SKULL_STATE_LLVM.current_func == SKULL_STATE_LLVM.main_func :
-		var->is_global;
-
-	if (var->is_const && (!is_global || is_const_literal)) {
+static void assign_value_to_var(LLVMValueRef value, Variable *var) {
+	if (var->is_const && (!var->is_global || LLVMIsConstant(value))) {
 		var->ref = value;
 	}
-	else if (!is_global || !is_first_assign || !is_const_literal) {
+	else {
+		// value isnt constant, so we add a store instruction
 		LLVMValueRef store = LLVMBuildStore(
 			SKULL_STATE_LLVM.builder,
 			value,
@@ -107,4 +100,22 @@ static void assign_value_to_var(
 
 		add_llvm_debug_info(store, &var->location);
 	}
+}
+
+static bool is_top_level(void) {
+	return SKULL_STATE_LLVM.current_func == SKULL_STATE_LLVM.main_func;
+}
+
+/*
+Determine if a global LLVM declaration is needed for this variable.
+
+Exported variables always need a declaration, and mutable variables only
+need one if they are marked "mut" (since they could be reassigned later).
+*/
+static bool requires_global_decl(const Variable *var) {
+	return var->is_exported || (var->is_global && !var->is_const);
+}
+
+static bool requires_alloca_decl(const Variable *var) {
+	return !var->is_global && !var->is_const;
 }
