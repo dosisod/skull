1e53c3f6e98251cdbbd8ea883537e4abffa71398
dosisod <39638017+dosisod@users.noreply.github.com>
2022-07-18 22:12:04 -0700

    Cleanup document generator

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -1,33 +1,34 @@
 from pathlib import Path
 import re
+from typing import DefaultDict, List
 
 re_comment = re.compile(r"\/\*[\s\S]*?\*\/")
 re_struct = re.compile(r"typedef\ struct\ .*\{(.*)\}")
 
+
 def iter_stack(code: str) -> int:
     # from: https://stackoverflow.com/a/38212061
-    index = 0
     stack = 0
 
     for i, c in enumerate(code):
-        index += 1
-        if c == '{':
+        if c == "{":
             stack += 1
 
-        elif c == '}':
+        elif c == "}":
             stack -= 1
 
             if stack == 0:
-                return index
+                return i + 1
 
     return 0
+
 
 def display_markdown(code: str) -> str:
     ret = ""
     for comment in re_comment.finditer(code):
         end = comment.end()
 
-        doc = code[(comment.start() + 3):(end - 3)]
+        doc = code[(comment.start() + 3) : (end - 3)]
         if "NOLINT" in doc:
             continue
 
@@ -35,25 +36,28 @@ def display_markdown(code: str) -> str:
         lines = code[end:].split("\n")
 
         if lines[1].startswith("typedef struct"):
-            struct_end = iter_stack(code[end + 1:])
+            struct_end = iter_stack(code[end + 1 :])
 
-            value = code[(end + 1):(end + struct_end + 1)]
+            value = code[(end + 1) : (end + struct_end + 1)]
 
         elif lines[1].startswith("typedef"):
             typedef_end = code[end:].index(";")
 
-            value = code[(end + 1):(end + typedef_end)]
+            value = code[(end + 1) : (end + typedef_end)]
 
         elif lines[1].startswith("#define"):
             define_end = code[end:].index("\n\n")
 
-            value = code[(end + 1):(end + define_end)]
+            value = code[(end + 1) : (end + define_end)]
 
         else:
             func_start = code[end:].index("{")
-            value = code[
-                (end + 1):(end + func_start - 1)
-            ].replace('\n', '').replace('\t', ' ').replace("( ", "(")
+            value = (
+                code[(end + 1) : (end + func_start - 1)]
+                .replace("\n", "")
+                .replace("\t", " ")
+                .replace("( ", "(")
+            )
 
         ret += f"```c\n{value}\n```\n\n"
 
@@ -67,26 +71,21 @@ def display_markdown(code: str) -> str:
 
     return ret
 
-units = {}
 
-sources = reversed(sorted([
-    src for src in Path("./skull").rglob("*") if src.suffix in [".c", ".h"]
-]))
+sources = sorted(
+    [src for src in Path("./skull").rglob("*") if src.suffix in [".c", ".h"]],
+    reverse=True,
+)
+
+units = DefaultDict[str, List[Path]](list)
 
 for source in sources:
-    filename = str(source)[:-2]
-
-    if filename in units:
-        units[filename].append(source)
-    else:
-        units[filename] = [source]
+    units[str(source.with_suffix(""))].append(source)
 
 for unit, files in units.items():
-    markdown = ""
-    for source in files:
-        with source.open() as f:
-            markdown += display_markdown(f.read())
+    markdown = "".join(display_markdown(f.read_text()) for f in files)
 
     if markdown:
-        with open(str(Path("docs") / source)[:-2] + ".md", "w+") as f:
-            f.write(f"# {unit}\n\n{markdown}")
+        (Path("docs") / files[0]).with_suffix(".md").write_text(
+            f"# {unit}\n\n{markdown}"
+        )
